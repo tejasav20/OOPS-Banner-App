5de8ee3b7331de0c0ed5f3ff42ed938fd3a9ba8d
Tejasav Agarwal <tejasavagarwal@Tejasavs-MacBook-Air-1197.local>
2026-03-10 09:22:15 +0530

    Coded to print OOPS using Arrays

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class OOPSBannerApp {
-		public static void main(String[] args)
-		{
-			System.out.println(String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "));
-			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "));
-			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "));
-			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "));
-			System.out.println(String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "));
-			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "));
-			System.out.println(String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   "));
-		}
+	public static void main(String[] args) 
+	{
+		lines[0] = System.out.println(String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "));
+		lines[1] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "));
+		lines[2] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "));
+		lines[3] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "));
+		lines[4] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "));
+		lines[5] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "));
+		lines[6] = System.out.println(String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   "));
+	}
 }
