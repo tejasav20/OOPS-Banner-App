d140d29d4002e1ee505ebccf9dec708748b73287
Tejasav Agarwal <tejasavagarwal@Tejasavs-MacBook-Air-1197.local>
2026-03-10 09:30:49 +0530

    Coded to print OOPS using Arrayinit

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class OOPSBannerApp {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		lines[0] = System.out.println(String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "));
-		lines[1] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "));
-		lines[2] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "));
-		lines[3] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "));
-		lines[4] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "));
-		lines[5] = System.out.println(String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "));
-		lines[6] = System.out.println(String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   "));
+		String[] lines = {
+			String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "),
+			String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "),
+			String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "),
+			String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "),
+			String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "),
+			String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "),
+			String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   ")};
+		for (String line : lines) {
+			System.out.println(line);
+		}
 	}
 }
