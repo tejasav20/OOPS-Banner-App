04e4ecafd8fc068130d3b649fc0dcf12a0b6ffa7
Tejasav Agarwal <tejasavagarwal@Tejasavs-MacBook-Air-1197.local>
2026-03-10 09:36:18 +0530

    Coded to print OOPS using static function

--- Main.java
+++ Main.java
@@ -1,16 +1,56 @@
 public class OOPSBannerApp {
-	public static void main(String[] args)
-	{
-		String[] lines = {
-			String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "),
-			String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "),
-			String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "),
-			String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "),
-			String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "),
-			String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "),
-			String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   ")};
-		for (String line : lines) {
-			System.out.println(line);
+	public static String[] getOPattern() {
+		return new String[] {
+			"   ***   ",
+			" **   ** ",
+			" **   ** ",
+			" **   ** ",
+			" **   ** ",
+			" **   ** ",
+			"   ***   ",
+		};
+	}
+	public static String[] getQPattern() {
+		return new String[] {
+			"   ***   ",
+			" **   ** ",
+			" **   ** ",
+			" **   ** ",
+			" **   ** ",
+			" **   ** ",
+			"   ***   ",
+		};
+	}
+	public static String[] getPPattern() {
+		return new String[] {
+			"   ******   ",
+			" **      ** ",
+			" **      ** ",
+			" **      ** ",
+			" *******    ",
+			" **         ",
+			" **         ",
+		};
+	}
+	public static String[] getSPattern() {
+		return new String[] {
+			"   ******   ",
+			" ***        ",
+			"  ***       ",
+			"    *****   ",
+			"       ***  ",
+			"       ***  ",
+			"   ******   ",
+		};
+	}
+	public static void main(String[] args) {
+		String[] oPattern = getOPattern();
+		String[] qPattern = getQPattern();
+		String[] pPattern = getPPattern();
+		String[] sPattern = getSPattern();
+		
+		for (int i = 0; i < oPattern.length; i++) {
+			System.out.println(oPattern[i]+ " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
 		}
 	}
 }
